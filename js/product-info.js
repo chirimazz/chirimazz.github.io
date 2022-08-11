69b4592bc44b63e9c1af6129d9d5f0bdaa8ea769
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-08-11 20:10:43 -0300

    version2

--- js/product-info.js
+++ js/product-info.js
@@ -1,9 +1,58 @@
 const URL="https://japceibal.github.io/emercado-api/cats_products/101.json"
-
+let categoriesArray = [];
 fetch(URL)
     .then(respuesta=>respuesta.json())
     .then(data=>{
         const div=document.querySelector("prod-list-container")
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend
     })
- 
+ //array donde se cargarán los datos recibidos:
+
+//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.productCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+/* 
+EJECUCIÓN:
+
+-Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
+-Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
