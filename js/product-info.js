2ebc324c584732b87bc810d5976e21f253c1cf33
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-08-11 21:43:08 -0300

    version2

--- js/product-info.js
+++ js/product-info.js
@@ -2,55 +2,19 @@ const URL="https://japceibal.github.io/emercado-api/cats_products/101.json"
 let categoriesArray = [];
 fetch(URL)
     .then(respuesta=>respuesta.json())
-    {
-        
-function showCategoriesList(array){
-    let htmlContentToAppend = "";
+    .then(data=>{
+        const img=document.querySelector('img');
+        const name=document.getElementById('name');
+        const currency=document.getElementById('currency');
+        const cost=document.getElementById('cost');
+        const description=document.getElementById('description');
+        const sold=document.getElementById('sold-count');
+        img.innerHTML=data[0].imgScr;
+        name.innerHTML=data[0].name;
+        currency.innerHTML=data[0].currency;
+        cost.innerHTML=data[0].cost;
+        description.innerHTML=data[0].description;
+        sold.innerHTML=data[0].soldCount;
 
-    for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name +`</h4> 
-                        <p> `+ category.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + category.productCount + ` artículos</small> 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
-    }
-}
-
-
-/* 
-EJECUCIÓN:
-
--Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
--Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
--Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
-
-*/
-
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(LIST_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
-        }
-    });
-});
-        
-    }
+    })
 
