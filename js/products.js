5e32d44c0c877cbe93510f871ed85ed2830d70f2
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-08-13 20:59:51 -0300

    version4

---
+++ js/products.js
@@ -0,0 +1,26 @@
+const ul = document.getElementById('productos');
+const URL= 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+  fetch(URL)
+.then((resp) => resp.json())
+.then(function(data) {
+    let autos = data.products;
+    return autos.map(function(auto) {
+        let li = createNode('li');
+        let img = createNode('img');
+        let span = createNode('span');
+
+        img.src = auto.image;
+        span.innerHTML = `${auto.name} ${auto.description}`;
+        append(li, img);
+        append(li, span);
+        append(ul, li);
+    })
+})
