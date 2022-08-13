932777b05c591d0098c171f8a42e594f6043b14d
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-08-12 22:15:27 -0300

    version3

--- js/product-info.js
+++ js/product-info.js
@@ -1,19 +1,27 @@
-const URL="https://japceibal.github.io/emercado-api/cats_products/101.json"
-fetch(URL)
-    .then(respuesta=>respuesta.json())
-    .then(data=>{
-      
-        const nombre=document.getElementById('name');
-        const moneda=document.getElementById('currency');
-        const costo=document.getElementById('cost');
-        const descr=document.getElementById('description');
-        const saldo=document.getElementById('sold-count');
- 
-        nombre.innerHTML= data[0].name;
-        moneda.innerHTML= data[0].currency;
-        costo.innerHTML= data[0].cost;
-        descr.innerHTML= data[0].description;
-        saldo.innerHTML= data[0].soldCount;
+const dl = document.getElementById('productos');
+const URL= 'https://japceibal.github.io/emercado-api/cats_products/101.json';
 
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+  fetch(URL)
+.then((resp) => resp.json())
+.then(function(data) {
+    let productos = data.results;
+    return productos.map(function(producto) {
+        let li = createNode('li');
+        let img = createNode('img');
+        let span = createNode('span');
+
+        img.src = producto.picture.medium;
+        span.innerHTML = `${productos.products.name} ${productos.products.description}`;
+        append(li, img);
+        append(li, span);
+        append(dl, li);
     })
+})
 
