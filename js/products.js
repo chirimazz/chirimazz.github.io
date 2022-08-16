c96fb96156af7c77c208e05b2b01473f8c7fbd5d
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-08-15 22:23:45 -0300

    version5
    lista de autos pronta

--- js/products.js
+++ js/products.js
@@ -1,26 +1,44 @@
-const ul = document.getElementById('productos');
-const URL= 'https://japceibal.github.io/emercado-api/cats_products/101.json';
 
-function createNode(element) {
-    return document.createElement(element);
+
+let URL= 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+
+let autosArray = [];
+function showProductList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let auto = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + auto.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ auto.name + auto.currency + auto.cost +`</h4> 
+                        <p> `+ auto.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + auto.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
 }
 
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-  fetch(URL)
-.then((resp) => resp.json())
-.then(function(data) {
-    let autos = data.products;
-    return autos.map(function(auto) {
-        let li = createNode('li');
-        let img = createNode('img');
-        let span = createNode('span');
 
-        img.src = auto.image;
-        span.innerHTML = `${auto.name} ${auto.description}`;
-        append(li, img);
-        append(li, span);
-        append(ul, li);
+
+document.addEventListener("DOMContentLoaded", function(e){
+    fetch(URL)
+    .then(response => response.json())
+    .then(data =>{
+        autosArray = data.products;
+        showProductList(autosArray);
     })
-})
+        }
+    )
