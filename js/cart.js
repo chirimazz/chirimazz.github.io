3808c5d4a780f19a817b95535a5d1c7e22f2de40
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-10-07 21:11:03 -0300

    carts.js
    function para mostrar elementos

---
+++ js/cart.js
@@ -0,0 +1,57 @@
+let cartElement={};
+
+document.addEventListener("DOMContentLoaded", async function(e){
+    try{
+const response = await fetch(CART_INFO_URL+25801+EXT_TYPE);
+const data= await response.json();
+cartElement = data
+console.log(cartElement)
+showcartElement();
+   
+
+}catch(error){
+        console.log(error)
+    }
+});   
+    
+    
+
+  
+
+    function showcartElement(){
+        let precioPorUni = cartElement.articles[0].unitCost;
+
+        listaCart=`
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Costo</th>
+      <th scope="col">Cantidad</th>
+      <th scope="col">Subtotal</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><img src="${cartElement.articles[0].image}" alt="product image class="min-vw 10:10vw " width="50"></th>
+      <td>${cartElement.articles[0].name}</td>
+      <td>${precioPorUni}</td>
+      <td><input type="number" value-1 id="cantArticulo" onchange="subtotal()"></td>
+      <td id="subtotal">${precioPorUni}</td>
+    </tr>
+   
+  </tbody>
+</table>
+
+`
+document.getElementById('compra').innerHTML=listaCart;
+    }
+
+
+
+function subtotal(){
+    let precioPorUni = cartElement.articles[0].unitCost;
+    document.getElementById("subtotal").innerHTML = precioPorUni * document.getElementById("cantArticulo").value;
+}
+    
