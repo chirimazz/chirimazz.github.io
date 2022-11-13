0164691d72bb8fe02fbf71f66906d8fe52f54a9e
chirimazz <110352031+chirimazz@users.noreply.github.com>
2022-11-13 13:26:52 -0300

    elimine el inicio de sesion

--- js/index.js
+++ js/index.js
@@ -12,36 +12,6 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 
-    iniciarSesion() /*llamar al elemento del logalSorge*/
+
 });
 
-
-let usuario = localStorage.getItem('user')
-function iniciarSesion(){
-    if(localStorage.getItem('user')=="Ingresar"){
-        document.getElementById('registrado').innerHTML="Ingresar";
-    }else{
-        document.getElementById("registrado").innerHTML=`
-            <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-        `+usuario+`
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
-        <li><a class="dropdown-item" href= "my-profile.html">Mi perfil</a></li>
-        <li><a class="dropdown-item" href="login.html" onclick = "cerrarSesion()" id="cerrarSesion">Cerrar Sesión</a></li>
-      </ul>
-    </div>`;
-    }
-    
-    }
-
-
-function cerrarSesion(){
-
-    localStorage.setItem("user","Ingresar");
-    window.location = "login.html";
-   
-
-
-}
